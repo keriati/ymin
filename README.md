6dec78bba95ba13affc4f441019cd0fe0bc117fa
Attila Kerekes <k.attila@gmail.com>
2012-04-20 11:34:53 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript / CSS minifier script for YUI Compressor
+
+This will just compress all .js or .css files in the directory, and output them into .min.js and .min.css files.
---
+++ ymin.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+##
+# JS/CSS Minifier script for YUI Compressor
+# Feel free to make it better :)
+##
+
+yuicompressor="~/bin/yuicompressor-2.4.7.jar"
+
+minjs()
+{
+    echo "Compressing all javascript files:"
+    for filein in `find . -name "*.js" | grep -v \.min\.js`
+    do
+        compress $filein
+    done
+    echo "Finished!"
+}
+
+mincss()
+{
+    echo "Compressing all css files:"
+    for filein in `find . -name "*.css" | grep -v \.min\.css`
+    do
+        compress $filein
+    done
+    echo "Finished!"
+}
+
+compress()
+{
+    filein=$1
+    fileout=${filein/\.css/\.min\.css}
+    fileout=${filein/\.js/\.min\.js}
+    #java -jar $yuicompressor $filein > $fileout
+    echo "  $filein => $fileout"
+}
+
+help()
+{
+    echo "JS/CSS Minifier script for YUI Compressor"
+    echo "Options"
+    echo ""
+    echo " -a     Compress all files in directory"
+    echo " <file> Compress one file in directory"
+}
+
+case $1 in
+    "-a")
+        minjs
+        mincss
+    ;;
+    *)
+        if [ -f $1 ]; then
+            compress $1
+        else
+            help
+        fi
+    ;;
+    "")
+        help
+    ;;
+esac
